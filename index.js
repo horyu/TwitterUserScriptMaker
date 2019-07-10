aba5c878b5bccc631b6251e0602ff273bc5eb0ad
horyu <28817604+horyu@users.noreply.github.com>
2019-07-10 14:00:48 +0900

    fix favicon

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const favicon = require('serve-favicon');
 
 const app = express();
 
@@ -8,6 +9,7 @@ app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public','favicon.ico')));
 
 app.get('/makescript.user.js', (req, res) => {
   const datas = req.query.datas || '[]'; //JSON.stringify(datas) : string で送られてくる
